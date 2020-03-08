18b1a540ab1f802f6f5310589bb5db088e6bc52b
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2020-03-08 11:46:34 +0330

    edit interface

--- Base/Abstraction/IBaseRepository.cs
+++ Base/Abstraction/IBaseRepository.cs
@@ -9,12 +9,14 @@ namespace CharCode.Base.Abstraction
     public interface IBaseRepository<T, TKey> where T : IModel<TKey>
     {
         Task DeleteAsync(TKey id);
+        Task DeleteAsync(IEnumerable<TKey> ids);
         Task<T> GetAsync(TKey id);
         Task<List<T>> GetAsync(PaginationConfig config);
         Task<long> GetCountAsync(PaginationConfig config);
         Task<T> InsertAsync(T entity);
         Task InsertAsync(IEnumerable<T> entities);
         Task UpdateAsync(TKey id, T entity);
+        Task UpdateAsync(IEnumerable<T> entities);
     }
     public interface IBaseRepository<T> : IBaseRepository<T, long> where T : IModel
     {
